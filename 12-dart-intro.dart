063a70dffea92b7d108e1a2c891b177f9f79fb98
andre <andresdaniel.go2020@gmail.com>
2023-07-16 20:49:12 -0500

    Adding

--- 12-dart-intro.dart
+++ 12-dart-intro.dart
@@ -1,6 +1,6 @@
 void main() {
   final mySquare = Square(side : 10);
-  mySquare.side = 5;
+  //mySquare.side = -10;
   print('area: ${mySquare.calculateArea()}');
 }
 
@@ -9,7 +9,8 @@ class Square{
   double _side;// side * side
   
   Square({required double side})
-    : _side = side;
+    : assert(side >=0),
+      _side = side;
   
   double get area{
     return _side * _side;
@@ -18,7 +19,6 @@ class Square{
   set side(double value){
     print('setting new value $value');
     if( value < 0) throw 'value must be >=0 ';
-  
     _side = value;
   }
   
@@ -27,3 +27,6 @@ class Square{
   }
 
 }
+
+
+
